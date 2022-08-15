d7f74245b1913a1a70823ee58f64db8a7bf19eb5
Vitor Sergio <vitorsergio111@gmail.com>
2022-08-15 01:33:38 -0300

    revrotate + int treatment

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
+/*   By: vsergio <vsergio@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 08:59:49 by vsergio           #+#    #+#             */
-/*   Updated: 2022/08/08 17:26:22 by vsergio          ###   ########.fr       */
+/*   Updated: 2022/08/15 00:44:11 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
 int	ft_atoi(char *str)
 {
-	int	res;
+	long int res;
 	int	i;
 	int	sign;
 
@@ -31,7 +32,26 @@ int	ft_atoi(char *str)
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		res = res * 10 + str[i] - '0';
+		if (res > 2147483647 && str[0] != '-')
+		{
+			write(2, "Error\n", 6);
+			exit(EXIT_FAILURE);
+		}
+		else if (res > 2147483648 && str[0] == '-')
+		{
+			write(2, "Error\n", 6);
+			exit(EXIT_FAILURE);
+		}
 		i++;
 	}
 	return (sign * res);
 }
+
+// int main(void)
+// {
+// 	char *str = "2147483648";
+// 	int tet = 2147483647;
+// 	int ret = ft_atoi(str);
+// 	printf("Retorno atoi: %i\n", ret);
+// 	printf("Valor inteiro: %i\n", tet * 2);
+// }
--- new.c
+++ new.c
@@ -13,6 +13,8 @@ void	pa(node_list **stack_a, node_list **stack_b);
 void	insert_last_node(node_list **stack, int value);
 void	insert_first_node(node_list **stack, node_list *first);
 void	pb(node_list **stack_a, node_list **stack_b);
+void	check_errors(int *numbers);
+
 int main(int argc, char **argv)
 {
 	node_list **stack_a;
@@ -20,38 +22,61 @@ int main(int argc, char **argv)
 	int *int_numbers;
 	int indexargv;
 	int indexnumbers;
-	int i;
+
 	stack_a = malloc(sizeof(node_list) * argc - 1);
 	stack_b = malloc(sizeof(node_list) * argc - 1);
-	int_numbers = malloc(400);
+	int_numbers = malloc(sizeof(int) * argc - 1);
 	indexargv = 1;
-	i = 0;
 	indexnumbers = 0;
 	while (indexargv < argc)
 	{
 		int_numbers[indexnumbers] = ft_atoi(argv[indexargv]);
+		check_errors(int_numbers);
 		insert_last_node(stack_a, int_numbers[indexnumbers]);
 		printf("Array de inteiros do atoi[%i]: Valor do no: %i\n", indexnumbers, int_numbers[indexnumbers]);
 		indexnumbers++;
 		indexargv++;
 	}
-	sa(stack_a);
+	// sa(stack_a);
 	print_stack_a(stack_a);
-	pb(stack_a, stack_b);
-	print_stack_a(stack_a);
-	print_stack_b(stack_b);
-	pb(stack_a, stack_b);
-	print_stack_a(stack_a);
-	print_stack_b(stack_b);
-	pa(stack_a, stack_b);
-	print_stack_b(stack_b);
-	print_stack_a(stack_a);
-	ra(stack_a);
-	print_stack_a(stack_a);
-	ra(stack_a);
-	print_stack_a(stack_a);
-	rra(stack_a);
-	print_stack_a(stack_a);
+	node_list *first;
+	first = *stack_a;
+	while(first)
+	{
+
+		first = first->next;
+	}
+	// pb(stack_a, stack_b);
+	// print_stack_a(stack_a);
+	// print_stack_b(stack_b);
+	// pb(stack_a, stack_b);
+	// print_stack_a(stack_a);
+	// print_stack_b(stack_b);
+	// pa(stack_a, stack_b);
+	// print_stack_b(stack_b);
+	// print_stack_a(stack_a);
+	// ra(stack_a);
+	// print_stack_a(stack_a);
+	// ra(stack_a);
+	// print_stack_a(stack_a);
+	// rra(stack_a);
+	// print_stack_a(stack_a);
+}
+
+void	check_errors(int *numbers)
+{
+	int i;
+
+	i = 0;
+	while(numbers[i])
+	{
+		if(numbers[i] == numbers[i + 1] || numbers[i] > 2147483647)
+		{
+			write(2, "Error\n", 6);
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
 }
 
 void	print_stack_a(node_list **stack_a)
@@ -91,6 +116,46 @@ void	swap(node_list **stack_list)
 	*stack_list = second;
 }
 
+void	push(node_list **sender, node_list **receiver)
+{
+	node_list *new_sender;
+	new_sender = (*sender)->next;
+	insert_first_node(receiver, *sender);
+	*sender = new_sender;
+}
+
+void	rotate(node_list **stack)
+{
+	node_list *first;
+	node_list *second;
+	node_list *last;
+
+	second = (*stack)->next;
+	first = *stack;
+	first->next = NULL;
+	last = get_last(second);
+	last->next = first;
+	*stack = second;
+}
+
+void	reverse_rotate(node_list **stack)
+{
+	node_list *prev;
+	node_list *last;
+	node_list *first;
+
+	first = *stack;
+	last = *stack;
+	while(last->next != NULL)
+	{
+		prev = last;
+		last = last->next;
+	}
+	prev->next = NULL;
+	last->next = first;
+	*stack = last;
+}
+
 void	sa(node_list **stack_a)
 {
 	swap(stack_a);
@@ -108,13 +173,6 @@ void	ss(node_list **stack_a, node_list **stack_b)
 	sa(stack_a);
 	sb(stack_b);
 }
-void	push(node_list **sender, node_list **receiver)
-{
-	node_list *new_sender;
-	new_sender = (*sender)->next;
-	insert_first_node(receiver, *sender);
-	*sender = new_sender;
-}
 
 void	pa(node_list **stack_a, node_list **stack_b)
 {
@@ -131,46 +189,40 @@ void	pb(node_list **stack_a, node_list **stack_b)
 	printf("pb\n");
 }
 
-void	shift_up(node_list **stack)
-{
-	node_list *first;
-	node_list *second;
-	node_list *last;
-
-	second = (*stack)->next;
-	first = *stack;
-	first->next = NULL;
-	last = get_last(second);
-	last->next = first;
-	*stack = second;
-}
-
-void	shift_down(node_list **stack)
-{
-	node_list *first;
-	node_list *last;
-	first = *stack;
-	last = get_last(first);
-	last->next = NULL;
-	insert_first_node(stack, last);
-}
-
 void	ra(node_list **stack_a)
 {
-	shift_up(stack_a);
+	rotate(stack_a);
 	printf("ra\n");
 }
 
 void	rb(node_list **stack_b)
 {
-	shift_up(stack_b);
+	rotate(stack_b);
 	printf("rb\n");
+}
+
+void	rr(node_list **stack_a, node_list **stack_b)
+{
+	ra(stack_a);
+	rb(stack_b);
 }
 
 void	rra(node_list **stack_a)
 {
-	shift_down(stack_a);
+	reverse_rotate(stack_a);
 	printf("rra\n");
+}
+
+void	rrb(node_list **stack_b)
+{
+	reverse_rotate(stack_b);
+	printf("rrb\n");
+}
+
+void	rrr(node_list **stack_a, node_list **stack_b)
+{
+	rra(stack_a);
+	rrb(stack_b);
 }
 
 // char	*grepvalue(hash_list *hash, int number)
